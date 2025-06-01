5d32c8406cf91f197db54ea39db862586895b6f3
Bruno Gonçalves de Souza <bruno.gsouza199@gmail.com>
2025-06-01 16:06:44 -0300

    Atualização na Apostila

---
+++ Udemy/Seção 12 - MongoDB/script.js
@@ -0,0 +1,17 @@
+// Comando utilizado para se conectar ao Banco de Dados.
+// use teste2
+
+// Comando utilizado para criar uma nova coleção.
+db.createCollection("collection")
+
+// Comando utilizado para listar os Banco de Dados criados no sistema.
+// show databases
+
+// Comando utilizado para listar as coleções criadas no Banco de Dados conectado.
+// show collections
+
+// use teste3
+db.createCollection("collection")
+
+// Comando utilizado para deletar um Banco de Dados.
+db.dropDatabase("teste3")
